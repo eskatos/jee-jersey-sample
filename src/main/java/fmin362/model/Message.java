f973a8247d24f3d21ffe4c8f3263a2a6d7849a58
Paul Merlin <paul@nosphere.org>
2012-10-17 23:25:43 +0200

    Message est maintenant une entité JPA

--- src/main/java/fmin362/model/Message.java
+++ src/main/java/fmin362/model/Message.java
@@ -1,18 +1,51 @@
 package fmin362.model;
 
+import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
 
+@Entity
 public class Message
+        implements Serializable
 {
 
-    private String text;
+    private static final long serialVersionUID = 1L;
 
-    private Date date;
+    @Id
+    @GeneratedValue( strategy = GenerationType.SEQUENCE )
+    private Long id;
+
+    @Column( length = 140 )
+    private String text = "";
+
+    @Column( name = "message_date" )
+    @Temporal( javax.persistence.TemporalType.DATE )
+    private Date date = new Date();
+
+    public Message()
+    {
+        // Constructeur par défaut nécessaire pour JPA
+    }
 
     public Message( String text, Date date )
     {
         this.text = text;
         this.date = date;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId( Long id )
+    {
+        this.id = id;
     }
 
     public Date getDate()
